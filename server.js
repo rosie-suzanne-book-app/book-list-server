0bfb16d8d457696572cc8ef5cf9751237764ca26
Suzanne Leber <79wabvlw1dt7@opayq.com>
2018-06-21 19:19:31 -0700

    Rearranged a few things. Stopping for now

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/******** THIS FILE SHOULD CONTAIN ********
+Dependencies: express, cors, pg
+    Middleware: app.use(cors())
+    API Endpoints:
+        app.get('/api/v1/books'
+    Client Endpoints:
+        app.get('*', (req, res) => res.status(403).send('This route does not exist'));
+*/
+
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
@@ -24,7 +33,7 @@ client.on('error', err => console.log(err));
 app.use(cors());
 
 // API Endpoints
-app.get('/api/v1/books', (req, res)=> {
+app.get('/api/v1/books', (req, res) => {
   console.log('OMG I am handling a GET request by a client!')
 
   let SQL = `SELECT * FROM books;`;
@@ -32,6 +41,16 @@ app.get('/api/v1/books', (req, res)=> {
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
+
+//get one book
+
+
+//get all books
+app.get(`api/v1/books`, (req, res) => {
+    let SQL = `SELECT * FROM books;`;
+    .then(results => res.send(results.rows))
+    .catch(consolelog)
+})
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
